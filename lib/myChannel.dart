df84f863d562053ef82125e3157f4d006d177840
dlwjdals <jml22zzang@gmail.com>
2022-05-24 19:23:48 +0900

    Add : myChannel - 페이지 이동 구현

--- lib/myChannel.dart
+++ lib/myChannel.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyChannel extends StatelessWidget {
+  const MyChannel({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              child: Text('asdf'),
-            ),
-            Container(),
-            Container(),
-          ],
-        ),
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Text('총5개'),
+          ),
+          Container(
+            child: Text('소개해주는 남자'),
+          ),
+          Container(
+            child: Text('tvN D ENT'),
+          ),
+        ],
       ),
     );
   }
